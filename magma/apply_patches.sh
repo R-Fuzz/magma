0fd0395730dac367836bf2fdc25e0b4cdd2a63c0
Haochen Zeng <haochen.zeng10@gmail.com>
2025-04-18 23:08:56 -0700

    patching early stop

--- magma/apply_patches.sh
+++ magma/apply_patches.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
+set -x
 
 ##
 # Pre-requirements:
 # - env TARGET: path to target work dir
 ##
 
-# TODO filter patches by target config.yaml
-find "$TARGET/patches/setup" "$TARGET/patches/bugs" -name "*.patch" | \
-while read patch; do
+# Stop immediately on any patch failure
+find "$TARGET/patches/setup" "$TARGET/patches/bugs" -name "*.patch" | while read patch; do
     echo "Applying $patch"
     name=${patch##*/}
     name=${name%.patch}
-    sed "s/%MAGMA_BUG%/$name/g" "$patch" | patch -p1 -d "$TARGET/repo"
+    sed "s/%MAGMA_BUG%/$name/g" "$patch" | patch -p1 -d "$TARGET/repo" || {
+        echo "❌ Failed to apply patch: $patch"
+        exit 1
+    }
 done
