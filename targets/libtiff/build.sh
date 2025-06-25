30f9c446a4879455e767fe7852ba6c731929616b
Haochen Zeng <haochen.zeng10@gmail.com>
2025-06-24 22:39:04 -0700

    build: allow autogen.sh to fail without aborting build

--- targets/libtiff/build.sh
+++ targets/libtiff/build.sh
@@ -19,7 +19,8 @@ mkdir -p "$WORK"
 mkdir -p "$WORK/lib" "$WORK/include"
 
 cd "$TARGET/repo"
-./autogen.sh
+(set +e ; ./autogen.sh) || \
+echo "autogen.sh failed to grab config.guess and config.sub from upstream master, continuing anyway"
 ./configure --disable-shared --prefix="$WORK" \
     --disable-lzma --disable-jpeg
 make -j$(nproc) clean
