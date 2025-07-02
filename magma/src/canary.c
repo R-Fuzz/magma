f53ecb447adf7e043487f891789cdd9573085883
Haochen Zeng <haochen.zeng10@gmail.com>
2025-07-01 17:12:57 -0700

    output bug reaching and triggering info to stderr; disable sleeping if bug is triggered.

--- magma/src/canary.c
+++ magma/src/canary.c
@@ -57,6 +57,9 @@ static bool magma_init(void)
 
 void magma_log(const char *bug, int condition)
 {
+    fprintf(stderr, "MAGMA: Bug %s reached\n", bug);
+    if (condition) fprintf(stderr, "MAGMA: Bug %s triggered\n", bug);
+
 #ifndef MAGMA_DISABLE_CANARIES
     if (!data_ptr && !magma_init()) {
         goto fatal;
@@ -90,7 +93,6 @@ fatal: (void)0;
         pid = getpid();
     }
     if (condition){
-        sleep(5);
         kill(pid, ((bool)condition)*11);
     }
 #endif
