d2ccea21687c3d137099a2db0b02c36f9a90876e
Haochen Zeng <haochen.zeng10@gmail.com>
2025-07-10 23:17:17 -0700

    fix openssl for aflgo

--- targets/openssl/build.sh
+++ targets/openssl/build.sh
@@ -13,7 +13,6 @@ if [ ! -d "$TARGET/repo" ]; then
     exit 1
 fi
 
-# build the libpng library
 cd "$TARGET/repo"
 
 CONFIGURE_FLAGS="no-asm"
@@ -21,14 +20,17 @@ CONFIGURE_FLAGS="no-asm"
 # the config script supports env var LDLIBS instead of LIBS
 export LDLIBS="$LIBS"
 
-$AR rcs "${FUZZER_LIB}.a" "$FUZZER_LIB"
+ADDTIONAL_FLAGS=""
+if [ -f "$FUZZER_LIB" ]; then
+    $AR rcs "${FUZZER_LIB}.a" "$FUZZER_LIB"
+    ADDTIONAL_FLAGS="--with-fuzzer-lib=$FUZZER_LIB"
+fi
 
-./config --debug enable-fuzz-libfuzzer disable-tests -DPEDANTIC \
-    --with-fuzzer-lib=$FUZZER_LIB \
+./config --debug enable-fuzz-libfuzzer enable-fuzz-afl disable-tests -DPEDANTIC $ADDTIONAL_FLAGS \
     -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION no-shared no-module \
     enable-tls1_3 enable-rc5 enable-md2 enable-ec_nistp_64_gcc_128 enable-ssl3 \
     enable-ssl3-method enable-nextprotoneg enable-weak-ssl-ciphers \
-    $CFLAGS -fno-sanitize=alignment $CONFIGURE_FLAGS
+    -fno-sanitize=alignment $CONFIGURE_FLAGS
 
 make -j$(nproc) clean
 make -j$(nproc) LDCMD="$CXX $CXXFLAGS"
