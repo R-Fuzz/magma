058304d18c6eb8c6e6bf6c0acfcdfff87f493a41
Haochen Zeng <haochen.zeng10@gmail.com>
2025-07-28 17:43:38 -0700

    adapt new file format for llm dockers

--- fuzzers/llm/run.sh
+++ fuzzers/llm/run.sh
@@ -8,21 +8,18 @@
 # - env SHARED: path to directory shared with host (to store results)
 # - env PROGRAM: name of program to run (should be found in $OUT)
 # - env ARGS: extra arguments to pass to the program
-# - env FUZZARGS: extra arguments to pass to the fuzzer
-# - env BUGID: ID of the bug under $PROGRAM
 # - env COMMIT: git commit hash of mazerunner
 # - env LLM_MODEL: name of the LLM model to use
 ##
 
 ARGS=${ARGS:-"@@"}
 
-pushd "$FUZZER/symsan"
+cd "$FUZZER/symsan"
 git fetch --all
 git reset --hard origin/main
 if [ -n "$COMMIT" ]; then
     git checkout "$COMMIT"
 fi
-popd
 
 mkdir -p "$SHARED/findings"
 cd "$SHARED/findings"
@@ -33,6 +30,14 @@ while read patch; do
     echo "Preparing env for $patch"
     NAME=${patch##*/}
     BUGID=${NAME%.patch}
+
+    pushd "$TARGET/BBtargets/${BUGID}"
+    cp ${PROGRAM}_policy.txt policy.txt
+    cp ${PROGRAM}_distance.cfg.txt distance.cfg.txt
+    cp ${PROGRAM}_bid_loc_mapping.txt bid_loc_mapping.txt
+    cp ${PROGRAM}_function_info.txt function_info.txt
+    popd
+
     PROGRAM_PATH="$OUT/clang_bc/$(basename "$TARGET")/${PROGRAM}"
 
     if [ ! -f "$PROGRAM_PATH" ]; then
@@ -40,10 +45,12 @@ while read patch; do
         continue
     fi
     
+    cd "$SHARED/findings"
     python3 "$FUZZER/symsan/mazerunner/llm_baseline.py" \
         -s "$TARGET/BBtargets/${BUGID}" \
         -m "$LLM_MODEL" \
-        -l prompt_${BUGID}.log -info ${BUGID} \
+        -l prompt_${BUGID}.log \
+        -info ${BUGID} \
         -- "$PROGRAM_PATH" $ARGS
 
     LLM_FILE="$SHARED/findings/llm_${BUGID}"
