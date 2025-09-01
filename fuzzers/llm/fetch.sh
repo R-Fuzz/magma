63a6d339c659accbfe85152dafb50eeb0cd4401a
Haochen Zeng <haochen.zeng10@gmail.com>
2025-09-01 15:06:39 -0700

    add directed_property_based_fuzzer repo

--- fuzzers/llm/fetch.sh
+++ fuzzers/llm/fetch.sh
@@ -8,6 +8,8 @@ set -e
 
 git clone -b mzt https://github.com/sgzeng/kernel-analyzer.git "$FUZZER/kernel-analyzer"
 
+git clone https://${GITHUB_TOKEN}@github.com/sgzeng/directed_property_based_fuzzer.git"$FUZZER/symsan"
+
 cd "$FUZZER"
 wget https://github.com/madler/zlib/archive/refs/tags/v1.2.13.tar.gz
 wget https://ftp.gnu.org/gnu/readline/readline-8.1.2.tar.gz
